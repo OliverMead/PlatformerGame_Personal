d4e537806a8b1226ebac1b6d0036f7bd02663ed1
Oliver Mead <oliver.j.mead@protonmail.com>
2018-10-04 10:35:02 +0100

    added line to hide the mouse pointer

--- World.gd
+++ World.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 func _ready():
-    set_process(true)
+	set_process(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(delta):
-   if Input.is_action_pressed("ui_cancel") or Input.is_key_pressed(KEY_Q):
-      get_tree().quit()
+	if Input.is_action_pressed("ui_cancel") or Input.is_key_pressed(KEY_Q):
+		get_tree().quit()
