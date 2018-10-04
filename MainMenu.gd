e91b11db1bd53cd4da8b171af432eb239ae21550
Oliver Mead <oliver.j.mead@protonmail.com>
2018-10-04 14:16:45 +0100

    added keyboard functionality in the main menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -10,7 +10,11 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass
 
-#func _process(delta):
+func _process(delta):
+	if Input.is_key_pressed(KEY_1):
+		get_tree().change_scene("res://World.tscn")
+	elif Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
